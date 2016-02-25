c3ab534e151bc6a24da622acb7f6c49e1158b562
thruthesky <thruthesky@gmail.com>
2016-02-25 20:01:03 +0900

    importing philgo desgin

---
+++ my-slider/my-slider.php
@@ -0,0 +1,31 @@
+<div class="container">
+    <div id="slider">
+        <nav>
+            <i class="fa fa-angle-left"></i>
+            <i class="fa fa-angle-right"></i>
+        </nav>
+        <ul>
+            <li style="z-index:1;">
+                <img src="<?php tpl() ?>/my-slider/img/i1.jpg">
+                <p class="content">
+                    원문한의원입니다. 보니 파시오에 있습니당 ~~
+                    <a href="#">This is link</a>
+                </p>
+            </li>
+            <li>
+                <img src="<?php tpl() ?>/my-slider/img/jae-su-1.jpg">
+                <p class="content">
+                    원문 한의원에 오신 것을 환영합니다.
+                    <a href="#">This is link</a>
+                </p>
+            </li>
+            <li>
+                <img src="<?php tpl() ?>/my-slider/img/su-1-2.jpg">
+                <p class="content">
+                    안녕하십니까.
+                    <a href="#">This is link</a>
+                </p>
+            </li>
+        </ul>
+    </div><!-- end of slider -->
+</div><!-- end of container -->
